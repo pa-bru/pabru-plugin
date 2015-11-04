aa75a8650231134a58678e17d55ae767ec8c373f
pa-bru <p.a.bru78@gmail.com>
2015-11-04 18:24:35 +0100

    add translation
    ajout de l'hook pour charger les fichiers de traduction

--- pabru-plugin.php
+++ pabru-plugin.php
@@ -16,3 +16,9 @@ add_filter('the_content', 'my_function');
 function my_function($content){
 	return str_replace('test', 'coucou', $content);
 }
+
+
+function my_plugin_load_plugin_textdomain() {
+    load_plugin_textdomain( 'my-plugin', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'my_plugin_load_plugin_textdomain' );
